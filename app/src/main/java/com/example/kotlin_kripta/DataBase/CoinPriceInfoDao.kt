3a4ebb4a9ad612162c0ba34ed0aaca4382130476
DmitriiRelin <relin_dmitrii@mail.ru>
2021-06-15 11:43:27 +0300

    Room(Test)

---
+++ app/src/main/java/com/example/kotlin_kripta/DataBase/CoinPriceInfoDao.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_kripta.DataBase
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.kotlin_kripta.pojo.CoinPriceInfo
+
+@Dao
+interface CoinPriceInfoDao {
+
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate")
+    fun getPriceList() : LiveData<List<CoinPriceInfo>>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSum LIMIT 1")
+    fun getPriceInfoAboutCoin(fSum: String): LiveData<CoinPriceInfo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPriceList(priceList: List<CoinPriceInfo>)
+
+}
